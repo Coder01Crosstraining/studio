0d47c97740be656fc59c5679b4ccf40c565dfa5e
yesid jimenez (Negocios en red) <negocioaconquistar@gmail.com>
2025-06-25 02:57:30 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -93,15 +93,16 @@ export function SidebarNav() {
         <SidebarMenu>
           {filteredNavItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href} legacyBehavior passHref>
-                <SidebarMenuButton
-                  isActive={pathname === item.href}
-                  tooltip={item.label}
-                >
+              <SidebarMenuButton
+                asChild
+                isActive={pathname === item.href}
+                tooltip={item.label}
+              >
+                <Link href={item.href}>
                   <item.icon />
                   <span>{item.label}</span>
-                </SidebarMenuButton>
-              </Link>
+                </Link>
+              </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
