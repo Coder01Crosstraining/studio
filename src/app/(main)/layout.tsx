9c38cd8a29c8d970b897c497dc5e520c930284f4
Asesor Online <crosstrainingasesoronline@gmail.com>
2025-07-03 22:10:11 +0000

    haz que se expanda mucho mas el contenido de todos los paneles: en la im

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -37,7 +37,7 @@ function MainContent({ children }: { children: React.ReactNode }) {
             shouldBlur && "blur-sm"
         )}>
             <MobileHeader />
-            <main className="flex-1 p-4 sm:p-6 lg:p-8">
+            <main className="flex-1 w-full mx-auto max-w-7xl p-4 sm:p-6 lg:p-8">
                 {children}
             </main>
         </div>
